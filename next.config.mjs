35b7a2d83c7947c8acbe136ad2dea0cc48180e87
Harsh Deep Rao <harshurao058@gmail.com>
2025-06-21 15:10:59 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+     output: "export",
+  trailingSlash: true,
+    basePath: '/myapp',
+
+};
 
 export default nextConfig;
